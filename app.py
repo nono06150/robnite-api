157b4f45ddbfb1e544d693d13c94708b1f55ffe0
nono06150 <ayouzymarwan11@gmail.com>
2026-04-26 12:26:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 from flask import Flask, jsonify, send_from_directory, request
-import os
 
 app = Flask(__name__)
 
 API_KEY = "ROBNITE_SECURE_KEY"
 
-UPDATES_FOLDER = "updates"
-
-
 @app.route("/")
 def home():
     return "Robnite API is running"
 
-
 @app.route("/version")
 def version():
     if request.headers.get("X-API-KEY") != API_KEY:
-        return jsonify({"error": "Unauthorized"}), 403
+        return jsonify({"error": "Unauthorized"}), 401
 
-    return send_from_directory(".", "version.json")
+    return jsonify({
+        "version": "1.0.1",
+        "files": [
+            "launcher.qml",
+            "splash.qml"
+        ]
+    })
 
-
-@app.route("/download/<filename>")
-def download_file(filename):
+@app.route("/download/<path:filename>")
+def download(filename):
     if request.headers.get("X-API-KEY") != API_KEY:
-        return jsonify({"error": "Unauthorized"}), 403
+        return jsonify({"error": "Unauthorized"}), 401
 
-    return send_from_directory(UPDATES_FOLDER, filename)
-
+    return send_from_directory("updates", filename)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
